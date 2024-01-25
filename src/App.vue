4cc63fbb14d041f41e2a1854807c4a52282f0324
Dustin Wright <dim3tapp@gmail.com>
2024-01-24 20:49:27 -0500

    Updated available missions

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <Header :header="this.header" />
   <div class="content-container">
-    <section class="section-container" id="missions" style="width:435px; height:714px;">
+    <section
+      class="section-container"
+      id="missions"
+      style="width: 435px; height: 714px"
+    >
       <div class="section-header clipped-medium-backward">
         <img src="/icons/mission-icon.svg" />
         <h1>Mission Log</h1>
@@ -21,7 +25,11 @@
         </div>
       </div>
     </section>
-    <section class="section-container" id="events" style="width:435px; height:714px;">
+    <section
+      class="section-container"
+      id="events"
+      style="width: 435px; height: 714px"
+    >
       <div class="section-header clipped-medium-backward">
         <img src="/icons/events-icon.svg" />
         <h1>Events Log</h1>
@@ -30,8 +38,12 @@
         <Markdown :source="events" class="markdown" />
       </div>
     </section>
-    <section class="section-container" id="pilots" style="width:894px; height:714px;">
-      <div style="height:52px; overflow:hidden;">
+    <section
+      class="section-container"
+      id="pilots"
+      style="width: 894px; height: 714px"
+    >
+      <div style="height: 52px; overflow: hidden">
         <div class="section-header clipped-medium-backward-pilot">
           <img src="/icons/pilot-icon.svg" />
           <h1>Pilot Roster</h1>
@@ -46,7 +58,7 @@
     </section>
   </div>
   <svg
-    style="visibility: hidden; position: absolute;"
+    style="visibility: hidden; position: absolute"
     width="0"
     height="0"
     xmlns="http://www.w3.org/2000/svg"
@@ -68,15 +80,15 @@
   <audio autoplay>
     <source src="/startup.ogg" type="audio/ogg" />
   </audio>
-  <Footer/>
+  <Footer />
 </template>
 
 <script>
-import Header from './components/layout/Header.vue';
-import Footer from './components/layout/Footer.vue';
-import Mission from './components/Mission.vue';
-import Pilot from './components/Pilot.vue';
-import Markdown from 'vue3-markdown-it';
+import Header from "./components/layout/Header.vue";
+import Footer from "./components/layout/Footer.vue";
+import Mission from "./components/Mission.vue";
+import Pilot from "./components/Pilot.vue";
+import Markdown from "vue3-markdown-it";
 
 export default {
   components: {
@@ -84,122 +96,122 @@ export default {
     Footer,
     Mission,
     Pilot,
-    Markdown
+    Markdown,
   },
 
   data() {
     return {
-      "mission_slug": "001",
-      "current_md": "",
-      "events": "",
-      "missions": [
+      mission_slug: "001",
+      current_md: "",
+      events: "",
+      missions: [
         {
-          "slug": "001",
-          "name": "OPERATION?CARDHOLDER",
-          "status": "start"
+          slug: "001",
+          name: "OPERATION?CARDHOLDER",
+          status: "success",
+        },
+        {
+          slug: "002",
+          name: "OPERATION?ASTEROID HOPPING",
+          status: "start",
         },
       ],
-      "pilots": [
+      pilots: [
         {
-          "callsign": "OMEN",
-          "alias": "Zacharias Morrison",
-          "code": "462370be-bd0f-41c2-b667-cc75f3a59a96///NDL-C-DEEP-STATION//377308ad-ba23-410b-ae37-68a1fb5f8db4",
-          "corpro": "SSC",
-          "frame": "Monarch",
-          "mech": "Brilliant Raptor"
+          callsign: "OMEN",
+          alias: "Zacharias Morrison",
+          code: "462370be-bd0f-41c2-b667-cc75f3a59a96///NDL-C-DEEP-STATION//377308ad-ba23-410b-ae37-68a1fb5f8db4",
+          corpro: "SSC",
+          frame: "Monarch",
+          mech: "Brilliant Raptor",
         },
         {
-          "callsign": "THORN",
-          "alias": "Ferrous Foss",
-          "code": "7cd700cc-c990-48ed-892f-e5468de724c4///NDL-C-DEEP-STATION//a98c3e28-ad4a-4f89-bcd9-501464e960da",
-          "corpro": "IPS-N",
-          "frame": "Blackbeard",
-          "mech": "Conjoined Torment"
+          callsign: "THORN",
+          alias: "Ferrous Foss",
+          code: "7cd700cc-c990-48ed-892f-e5468de724c4///NDL-C-DEEP-STATION//a98c3e28-ad4a-4f89-bcd9-501464e960da",
+          corpro: "IPS-N",
+          frame: "Blackbeard",
+          mech: "Conjoined Torment",
         },
         {
-          "callsign": "STARCHILD",
-          "alias": "Jesk Oranzo",
-          "code": "4be26ce9-923b-4069-b6c9-76437d4be455///NDL-C-DEEP-STATION//056940c6-8d55-4190-8e85-57caa043cb1a",
-          "corpro": "HORUS",
-          "frame": "Gorgon",
-          "mech": "Last Vision"
+          callsign: "STARCHILD",
+          alias: "Jesk Oranzo",
+          code: "4be26ce9-923b-4069-b6c9-76437d4be455///NDL-C-DEEP-STATION//056940c6-8d55-4190-8e85-57caa043cb1a",
+          corpro: "HORUS",
+          frame: "Gorgon",
+          mech: "Last Vision",
         },
         {
-          "callsign": "PROPHET",
-          "alias": "Greva Banard",
-          "code": "98ca9616-044e-4f87-b89b-aae4eb3387ec///NDL-C-DEEP-STATION//6f572259-6946-41bf-931a-e0543709e892",
-          "corpro": "HA",
-          "frame": "Barbarossa",
-          "mech": "MACHINE GOD"
+          callsign: "PROPHET",
+          alias: "Greva Banard",
+          code: "98ca9616-044e-4f87-b89b-aae4eb3387ec///NDL-C-DEEP-STATION//6f572259-6946-41bf-931a-e0543709e892",
+          corpro: "HA",
+          frame: "Barbarossa",
+          mech: "MACHINE GOD",
         },
       ],
-      "header": {
-        "planet": "Hercynia",
-        "year": "5014u",
-        "system": "Ardennes-3",
-        "gate": "Atlas-Quanokrim",
-        "ring": "Atlas-Line",
-        "headerTitle": "Mirrorsmoke",
-        "headerSubtitle": "Mercenary Company",
-        "subheaderTitle": "Crisis Response",
-        "subheaderSubtitle": "Delta-Echo-Echo-Zulu",
+      header: {
+        planet: "Hercynia",
+        year: "5014u",
+        system: "Ardennes-3",
+        gate: "Atlas-Quanokrim",
+        ring: "Atlas-Line",
+        headerTitle: "Mirrorsmoke",
+        headerSubtitle: "Mercenary Company",
+        subheaderTitle: "Crisis Response",
+        subheaderSubtitle: "Delta-Echo-Echo-Zulu",
       },
-      "options":{
-        "eventsMarkdownPerMission": true
-      }
-    }
+      options: {
+        eventsMarkdownPerMission: true,
+      },
+    };
   },
 
   created() {
-    this.loadMissionMarkdown()
-    this.loadEventsMarkdown()
+    this.loadMissionMarkdown();
+    this.loadEventsMarkdown();
   },
 
-  computed: {
-
-  },
+  computed: {},
 
   methods: {
     selectMission(mission) {
       this.mission_slug = mission.slug;
-      this.loadMissionMarkdown()
-      if(this.options.eventsMarkdownPerMission){
+      this.loadMissionMarkdown();
+      if (this.options.eventsMarkdownPerMission) {
         this.loadEventsMarkdown();
       }
     },
     loadMissionMarkdown() {
       let self = this;
-      let md = `/missions/${self.mission_slug}.md`
+      let md = `/missions/${self.mission_slug}.md`;
       var client = new XMLHttpRequest();
-      client.open('GET', md);
+      client.open("GET", md);
       client.onreadystatechange = function () {
         self.current_md = client.responseText;
-      }
+      };
       client.send();
     },
     loadEventsMarkdown() {
       let self = this;
       let md = "";
 
-      if(self.options.eventsMarkdownPerMission){
-        md = `/events/${self.mission_slug}.md`
-      }
-      else {
-        md = "/events.md"
+      if (self.options.eventsMarkdownPerMission) {
+        md = `/events/${self.mission_slug}.md`;
+      } else {
+        md = "/events.md";
       }
 
       var client = new XMLHttpRequest();
-      client.open('GET', md);
+      client.open("GET", md);
       client.onreadystatechange = function () {
         self.events = client.responseText;
-      }
+      };
       client.send();
-    }
-  }
-
-}
+    },
+  },
+};
 </script>
-
 
 <style lang="scss">
 #app {
